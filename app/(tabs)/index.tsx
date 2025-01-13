54d025ebb5e1f86b44be611d6f573323013737ce
Bkhamilton <bkhamilton310@gmail.com>
2025-01-12 23:20:00 -0500

    updated spacing, moved Home Screen component to its own file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,12 @@
 import { StyleSheet } from 'react-native';
+import HomeScreen from '@/components/Home/HomeScreen';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+export default function HomePage() {
+    return (
+        <> 
+            <HomeScreen />
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
---
+++ components/Home/HomeScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Text, View } from '@/components/Themed';
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={{ fontSize: 16 }}>Games</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingVertical: 8,
+    },
+});
