81af1270237f9fc4a98d45e5317173da904d265d
Bkhamilton <bkhamilton310@gmail.com>
2025-01-13 00:05:45 -0500

    added GameSelector to Home Screen

--- components/Home/HomeScreen.tsx
+++ components/Home/HomeScreen.tsx
@@ -4,14 +4,17 @@ import { Text, View } from '@/components/Themed';
 import SudokuGame from './Sudoku/SudokuGame';
 import DinoGame from './DinoRun/DinoRunGame';
 import WordSearchGame from './WordSearch/WordSearchGame';
+import GameSelector from './GameSelector/GameSelector';
 
 export default function HomeScreen() {
+
+    const gameTitles = ['Sudoku', 'Dino Run', 'Word Search'];
+
     return (
         <View style={styles.container}>
             <View>
                 <Text style={{ fontSize: 16 }}>Games</Text>
-                <WordSearchGame />
-
+                <GameSelector gameTitles={gameTitles} />
             </View>
         </View>
     );
