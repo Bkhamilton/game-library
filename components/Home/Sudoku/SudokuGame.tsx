bdc7d2ac88979f4b5c9dfe6f6b6fad2206b5ac8e
Bkhamilton <bkhamilton310@gmail.com>
2025-01-12 23:44:51 -0500

    imported generateSudoku function

--- components/Home/Sudoku/SudokuGame.tsx
+++ components/Home/Sudoku/SudokuGame.tsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native';
-
-const initialBoard = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-];
+import { generateSudokuPuzzle } from '@/utils/SudokuGenerator';
 
 export default function SudokuGame() {
-    const [board, setBoard] = useState(initialBoard);
+    const [board, setBoard] = useState(generateSudokuPuzzle('Hard'));
 
     const handleInputChange = (row: number, col: number, value: string) => {
         const newBoard = [...board];
