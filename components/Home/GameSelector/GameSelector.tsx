849fb22d23d498d5746ece5701e985373da08785
Bkhamilton <bkhamilton310@gmail.com>
2025-01-15 00:02:37 -0500

    updated onPress event to properly navigate to different games

--- components/Home/GameSelector/GameSelector.tsx
+++ components/Home/GameSelector/GameSelector.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import useTheme from '@/hooks/useTheme';
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 
 interface GameSelectorProps {
     gameTitles: string[];
@@ -11,11 +11,26 @@ const GameSelector: React.FC<GameSelectorProps> = ({ gameTitles }) => {
 
     const { text, grayBackground } = useTheme();
 
+    const router = useRouter();
+
+    const handleGamePress = (title: string) => {
+        switch (title) {
+            case 'Sudoku':
+                return router.push('/sudoku');
+            case 'Dino Run':
+                return router.push('/dinorun');
+            case 'Word Search':
+                return router.push('/wordSearch');
+            default:
+                return '';
+        }
+    }
+
     return (
         <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.scrollView}>
             {gameTitles.map((title, index) => (
                 <View key={index} style={styles.gameItem}>
-                    <TouchableOpacity onPress={() => console.log(title)}>
+                    <TouchableOpacity onPress={() => handleGamePress(title)}>
                         <View style={[styles.gameIcon, { backgroundColor: grayBackground }]} />
                         <Text style={[styles.gameTitle, { color: text }]}>{title}</Text>
                     </TouchableOpacity>
