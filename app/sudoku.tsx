ddad4d4f2a968068eeac2105990cd84a9c558103
Bkhamilton <bkhamilton310@gmail.com>
2025-01-15 00:09:58 -0500

    Refactor SudokuScreen component to use SudokuGame component

--- app/sudoku.tsx
+++ app/sudoku.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 
-export default function SudokuScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
+import SudokuGame from '@/components/Home/Sudoku/SudokuGame';
 
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </View>
-  );
+export default function SudokuScreen() {
+    return (
+        <View style={styles.container}>
+            <SudokuGame />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -22,14 +16,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
