39ca61af1771d6358a8f684c4db38f2779247075
Bkhamilton <bkhamilton310@gmail.com>
2025-01-16 16:02:09 -0500

    moved handleGamePress logic to HomeScreen component

--- components/Home/GameSelector/GameSelector.tsx
+++ components/Home/GameSelector/GameSelector.tsx
@@ -1,73 +1,57 @@
 import React from "react";
-import {
-  ScrollView,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+import { ScrollView, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text } from "@/components/Themed";
 import useTheme from "@/hooks/useTheme";
-import { Link, useRouter } from "expo-router";
 
 interface GameSelectorProps {
-  gameTitles: string[];
+    gameTitles: string[];
+    handleSelectGame: (game: string) => void;
 }
 
-const GameSelector: React.FC<GameSelectorProps> = ({ gameTitles }) => {
-  const { text, grayBackground } = useTheme();
+const GameSelector: React.FC<GameSelectorProps> = ({ gameTitles, handleSelectGame }) => {
+    const { grayBackground } = useTheme();
 
-  const router = useRouter();
+    const handleGamePress = (title: string) => {
+        handleSelectGame(title);
+    };
 
-  const handleGamePress = (title: string) => {
-    switch (title) {
-      case "Sudoku":
-        return router.push("/sudoku");
-      case "Dino Run":
-        return router.push("/dinorun");
-      case "Word Search":
-        return router.push("/wordSearch");
-      default:
-        return "";
-    }
-  };
-
-  return (
-    <ScrollView
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      style={styles.scrollView}
-    >
-      {gameTitles.map((title, index) => (
-        <View key={index} style={styles.gameItem}>
-          <TouchableOpacity onPress={() => handleGamePress(title)}>
-            <View
-              style={[styles.gameIcon, { backgroundColor: grayBackground }]}
-            />
-            <Text style={[styles.gameTitle, { color: text }]}>{title}</Text>
-          </TouchableOpacity>
-        </View>
-      ))}
-    </ScrollView>
-  );
+    return (
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={styles.scrollView}
+        >
+            {gameTitles.map((title, index) => (
+                <View key={index} style={styles.gameItem}>
+                    <TouchableOpacity onPress={() => handleGamePress(title)}>
+                        <View
+                            style={[styles.gameIcon, { backgroundColor: grayBackground }]}
+                        />
+                        <Text style={styles.gameTitle}>{title}</Text>
+                    </TouchableOpacity>
+                </View>
+            ))}
+        </ScrollView>
+    );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    paddingVertical: 10,
-  },
-  gameItem: {
-    alignItems: "center",
-    marginHorizontal: 10,
-  },
-  gameIcon: {
-    width: 100,
-    height: 100,
-    borderRadius: 10,
-    marginBottom: 5,
-  },
-  gameTitle: {
-    textAlign: "center",
-  },
+    scrollView: {
+        paddingVertical: 10,
+    },
+    gameItem: {
+        alignItems: "center",
+        marginHorizontal: 10,
+    },
+    gameIcon: {
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+        marginBottom: 5,
+    },
+    gameTitle: {
+        textAlign: "center",
+    },
 });
 
 export default GameSelector;
