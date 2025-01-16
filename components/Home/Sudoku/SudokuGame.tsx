4758c206fe7d0ad197ca219aedea57b261a5fb47
Bkhamilton <bkhamilton310@gmail.com>
2025-01-16 14:02:45 -0500

    added initialNumbers to determine which numbers are initally generated to prevent them from being removed

--- components/Home/Sudoku/SudokuGame.tsx
+++ components/Home/Sudoku/SudokuGame.tsx
@@ -9,9 +9,11 @@ import SudokuNumbers from './SudokuNumbers';
 export default function SudokuGame() {
     const [difficulty, setDifficulty] = useState('Hard');
     const [board, setBoard] = useState<number[][]>([]);
+    const [initialNumbers, setInitialNumbers] = useState<{ [key: string]: boolean }>({});
     const [selectedNumber, setSelectedNumber] = useState<number | null>(null);
 
     const handleInputChange = (row: number, col: number, value: string) => {
+        if (initialNumbers[`${row}-${col}`]) return; // Prevent changing initial numbers
         const newBoard = [...board];
         newBoard[row][col] = parseInt(value) || 0;
         setBoard(newBoard);
@@ -22,12 +24,22 @@ export default function SudokuGame() {
     };
 
     useEffect(() => {
-        setBoard(generateSudokuPuzzle(difficulty));
+        const newBoard = generateSudokuPuzzle(difficulty);
+        setBoard(newBoard);
+        const initialNums: { [key: string]: boolean } = {};
+        newBoard.forEach((row: any[], rowIndex: any) => {
+            row.forEach((cell: number, colIndex: any) => {
+                if (cell !== 0) {
+                    initialNums[`${rowIndex}-${colIndex}`] = true;
+                }
+            });
+        });
+        setInitialNumbers(initialNums);
     }, [difficulty]);
 
     return (
         <View style={styles.container}>
-            <SudokuBoard board={board} handleInputChange={handleInputChange} selectedNumber={selectedNumber}/>
+            <SudokuBoard board={board} handleInputChange={handleInputChange} selectedNumber={selectedNumber} initialNumbers={initialNumbers}/>
             <SudokuNumbers selectNumber={handleSelectNumber} />
         </View>
     );
