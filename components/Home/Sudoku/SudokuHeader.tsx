145d446286702d3a3545a6e4a5feb4b629adf851
Bkhamilton <bkhamilton310@gmail.com>
2025-01-22 19:07:51 -0500

    add Timer component to track elapsed time in game

--- components/Home/Sudoku/SudokuHeader.tsx
+++ components/Home/Sudoku/SudokuHeader.tsx
@@ -2,31 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import useTheme from '@/hooks/useTheme';
+import Timer from '../Helpers/Timer';
 
 export default function SudokuHeader({ wrongCount }: { wrongCount: number }) {
-    const [seconds, setSeconds] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setSeconds(prevSeconds => prevSeconds + 1);
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
-    const formatTime = (seconds: number) => {
-        const minutes = Math.floor(seconds / 60);
-        const remainingSeconds = seconds % 60;
-        return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-    };
-
     return (
         <View style={styles.container}>
             <View style={styles.innerContainer}>
                 <View>
 
                 </View>
-                <Text style={{ fontSize: 16 }}>Time: {formatTime(seconds)}</Text>
+                <Timer 
+                    isActive={true}
+                />
                 <View>
                     <Text style={{ fontSize: 16 }}>{wrongCount}/10</Text>
                 </View>
