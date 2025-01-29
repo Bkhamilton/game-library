35e8122457b2a7c7e35693c4f3fc13b01a178bcb
Kenneth <kmsullivanjr@uri.edu>
2025-01-28 21:20:20 -0500

    Changed name to ostrich haul

---
+++ app/ostrichhaul.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import OstrichHaul from "@/components/Home/OstrichHaul/OstrichHaulGame";
+
+export default function OstrichHaulScreen() {
+  return (
+    <>
+      <OstrichHaul />
+    </>
+  );
+}
