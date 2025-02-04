fb190fdfb8bfa4e300b148ed544389318cb494e2
Kenneth <kmsullivanjr@uri.edu>
2025-02-04 16:31:24 -0500

    changed from type to constant

--- constants/Types.ts
+++ constants/Types.ts
@@ -1,24 +1,25 @@
-export type GameTitle =
-  | "Sudoku"
-  | "Ostrich Haul"
-  | "Word Search"
-  | "Crossword"
-  | "Minesweeper"
-  | "GoGoBird";
+import sudokuLogo from "@/assets/images/gamelogo/sudokuLogo.jpg";
+import gogoBirdLogo from "@/assets/images/gamelogo/gogoBirdLogo.jpg";
+import wordSearchLogo from "@/assets/images/gamelogo/wordSearchLogo.jpg";
+import ostrichHaulLogo from "@/assets/images/gamelogo/ostrichHaulLogo.jpg";
+import crosswordLogo from "@/assets/images/gamelogo/crosswordLogo.jpg";
+import minesweeperLogo from "../assets/images/gamelogo/minesweeperLogo.jpg";
+
+export type GameTitle = "Sudoku" | "Ostrich Haul" | "Word Search" | "Crossword" | "Minesweeper" | "GoGoBird";
 
 export type Difficulty = "Easy" | "Medium" | "Hard" | "Extreme";
 
 export type Games = {
-  id: number;
-  title: string;
-  description: string;
+    id: number;
+    title: string;
+    description: string;
 };
 
-export type GameLogos = {
-  Sudoku: "@/assets/gamelogo/sudokuLogo.jpg";
-  GoGoBird: "@/assets/gamelogo/ostrichHaul.jpg";
-  WordSearch: "@/assets/gamelogo/crosswordLogo.jpg";
-  OstrichHaul: "@/assets/gamelogo/ostrichHaul.jpg";
-  Crossword: "@/assets/gamelogo/crosswordLogo.jpg";
-  Minesweeper: "@/assets/gamelogo/minesweeper.jpg";
+export const GameLogos = {
+    Sudoku: sudokuLogo,
+    GoGoBird: gogoBirdLogo,
+    "Word Search": wordSearchLogo,
+    "Ostrich Haul": ostrichHaulLogo,
+    Crossword: crosswordLogo,
+    Minesweeper: minesweeperLogo,
 };
