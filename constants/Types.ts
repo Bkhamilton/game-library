77c3c42fda09e4d753347cdc42cfe7951c9193e9
Kenneth <kmsullivanjr@uri.edu>
2025-02-04 15:58:23 -0500

    Added game logo's and added typses to constants file.

--- constants/Types.ts
+++ constants/Types.ts
@@ -1,9 +1,24 @@
-export type GameTitle = "Sudoku" | "Ostrich Haul" | "Word Search" | "Crossword" | "Minesweeper" | "GoGoBird";
+export type GameTitle =
+  | "Sudoku"
+  | "Ostrich Haul"
+  | "Word Search"
+  | "Crossword"
+  | "Minesweeper"
+  | "GoGoBird";
 
 export type Difficulty = "Easy" | "Medium" | "Hard" | "Extreme";
 
 export type Games = {
-    id: number;
-    title: string;
-    description: string;
+  id: number;
+  title: string;
+  description: string;
+};
+
+export type GameLogos = {
+  Sudoku: "@/assets/gamelogo/sudokuLogo.jpg";
+  GoGoBird: "@/assets/gamelogo/ostrichHaul.jpg";
+  WordSearch: "@/assets/gamelogo/crosswordLogo.jpg";
+  OstrichHaul: "@/assets/gamelogo/ostrichHaul.jpg";
+  Crossword: "@/assets/gamelogo/crosswordLogo.jpg";
+  Minesweeper: "@/assets/gamelogo/minesweeper.jpg";
 };
