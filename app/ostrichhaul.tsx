7903bf0b7b5f458eefe050dddee05c54fabbad07
Bkhamilton <bkhamilton310@gmail.com>
2025-02-22 17:40:05 -0500

    Code Cleanup

--- app/ostrichhaul.tsx
+++ app/ostrichhaul.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import OstrichHaul from "@/components/Home/OstrichHaul/OstrichHaulGame";
 
 export default function OstrichHaulScreen() {
-  return (
-    <>
-      <OstrichHaul />
-    </>
-  );
+    return (
+        <>
+            <OstrichHaul />
+        </>
+    );
 }
