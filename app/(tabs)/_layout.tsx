af44a9db132b5088b193a5ee0418120b70e60947
Kenneth <kmsullivanjr@uri.edu>
2025-02-27 19:29:33 -0500

    Slight Styling to tab bar and added a color constant

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable } from "react-native";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-import useTheme from '@/hooks/useTheme';
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import useTheme from "@/hooks/useTheme";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-    name: React.ComponentProps<typeof FontAwesome>['name'];
-    color: string;
-}) {
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>["name"]; color: string }) {
     return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
     const colorScheme = useColorScheme();
 
-    const { tint, primary } = useTheme();
+    const { tint, primary, tabBarInactiveColor } = useTheme();
 
     return (
         <Tabs
@@ -28,49 +25,45 @@ export default function TabLayout() {
                 // Disable the static render of the header on web
                 // to prevent a hydration error in React Navigation v6.
                 headerShown: useClientOnlyValue(false, true),
-                tabBarInactiveTintColor: primary,
+                tabBarInactiveTintColor: tabBarInactiveColor,
                 tabBarStyle: {
                     borderRadius: 32,
-                    backgroundColor: "gray",
+                    backgroundColor: "white",
                     position: "absolute",
-                    height: "8%",
+                    height: "7%",
                     paddingTop: "1%",
-                    marginBottom: "3%",
-                    width: "80%",
-                    marginHorizontal: "10%",
+                    marginBottom: "6%",
+                    width: "60%",
+                    marginHorizontal: "20%",
                     justifyContent: "center",
                     alignItems: "center",
-                    shadowColor: "black",
-                    shadowOffset: { width: 0, height: 2 },
+                    shadowColor: "#000",
+                    shadowOffset: {
+                        width: 2,
+                        height: 4,
+                    },
                     shadowOpacity: 0.25,
-                  },
-
-            }}>
+                    shadowRadius: 3.84,
+                    elevation: 5,
+                },
+            }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
-                title: 'Home',
-                tabBarIcon: ({ color }) => <TabBarIcon name="gamepad" color={color} />,
-                headerRight: () => (
-                    <Link href="/modal" asChild>
-                        <Pressable>
-                            {({ pressed }) => (
-                            <FontAwesome
-                                name="info-circle"
-                                size={25}
-                                color={Colors[colorScheme ?? 'light'].text}
-                                style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                            />
-                            )}
-                        </Pressable>
-                    </Link>
-                ),
+                    title: "Home",
+                    tabBarIcon: ({ color }) => <TabBarIcon name="gamepad" color={color} />,
+                    headerRight: () => (
+                        <Link href="/modal" asChild>
+                            <Pressable>{({ pressed }) => <FontAwesome name="info-circle" size={25} color={Colors[colorScheme ?? "light"].text} style={{ opacity: pressed ? 0.5 : 1 }} />}</Pressable>
+                        </Link>
+                    ),
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options={{
-                    title: 'Profile',
+                    title: "Profile",
                     tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
                     headerShown: false,
                 }}
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,33 +1,35 @@
-const tintColorLight = 'red';
-const tintColorDark = '#588157';
+const tintColorLight = "red";
+const tintColorDark = "#588157";
 
 export default {
     light: {
-        text: '#000',
-        background: '#fff',
-        grayBackground: '#F5F5F5',
-        grayBorder: '#EBEBEB',
+        text: "#000",
+        background: "#fff",
+        grayBackground: "#F5F5F5",
+        grayBorder: "#EBEBEB",
         border: "#000",
         tint: tintColorLight,
-        tabIconDefault: 'black',
+        tabIconDefault: "black",
         tabIconSelected: tintColorLight,
-        primary: '#588157',
-        secondary: '#6db36b',
-        tabBar: 'gray',
-        activeBackground: 'green' 
+        primary: "#588157",
+        secondary: "#6db36b",
+        tabBar: "gray",
+        activeBackground: "green",
+        tabBarInactiveColor: "gray",
     },
     dark: {
-        text: '#fff',
-        background: '#121212',
-        grayBackground: '#313131',
-        grayBorder: '#292929',
+        text: "#fff",
+        background: "#121212",
+        grayBackground: "#313131",
+        grayBorder: "#292929",
         border: "#fff",
         tint: tintColorDark,
-        tabIconDefault: '#ccc',
+        tabIconDefault: "#ccc",
         tabIconSelected: tintColorDark,
-        primary: '#19362d',
-        secondary: '#112921',
-        tabBar: 'white',
-        activeBackground: 'pink'
+        primary: "#19362d",
+        secondary: "#112921",
+        tabBar: "white",
+        activeBackground: "pink",
+        tabBarInactiveColor: "gray",
     },
 };
