a0222b2c1aa72ee571718d01d58f374ebf3a516e
Ben <bkhamilton310@gmail.com>
2025-02-28 21:19:27 -0500

    feat: add functions to insert high scores and game results

--- db/Scores/Scores.js
+++ db/Scores/Scores.js
@@ -15,13 +15,44 @@ export const getScores = async (db) => {
 export const insertScore = async (db, gameId, score, metric) => {
     try {
         const result = await db.runAsync('INSERT INTO Scores (gameId, score, metric) VALUES (?, ?, ?)', [gameId, score, metric]);
-        console.log(result);
         return result.lastInsertRowId;
     } catch (error) {
         console.error('Error inserting score:', error);
         throw error;
     }
 };
+
+// Function to insert a high score
+export const insertHighScore = async (db, gameId, score) => {
+    try {
+        const metric = 'highScore';
+        await insertScore(db, gameId, score, metric);
+    } catch (error) {
+        console.error('Error inserting high score:', error);
+        throw error;
+    }
+};
+
+// Function to insert a result (win or loss)
+export const insertResult = async (db, gameId, result) => {
+    try {
+        const metric = 'result';
+        await insertScore(db, gameId, result, metric);
+    } catch (error) {
+        console.error('Error inserting result:', error);
+        throw error;
+    }
+};
+
+// Function to insert a win
+export const insertWin = async (db, gameId) => {
+    await insertResult(db, gameId, 1);
+}
+
+// Function to insert a loss
+export const insertLoss = async (db, gameId) => {
+    await insertResult(db, gameId, 0);
+}
 
 // Function to update a score
 export const updateScore = async (db, scoreId, gameId, score, metric) => {
